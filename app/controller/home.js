1f80f3f56b4b43d80eb7f6762233ce3cd31cd5d4
Matatini Reia <shin_taku@hotmail.com>
2020-04-08 23:39:05 +0200

    home page is finished

--- app/controller/home.js
+++ app/controller/home.js
@@ -1,7 +1,7 @@
 const router = require('@koa/router')({});
 
 router.get('/', async ctx => {
-  await ctx.render('home');
+  await ctx.render('home/home');
 });
 
 module.exports = router;
--- app/controller/wp_options.js
+++ app/controller/wp_options.js
@@ -4,7 +4,7 @@ const Router = require('@koa/router');
 const router = new Router({prefix: '/wp-options'});
 
 router.get('/', async ctx => {
-  await ctx.render('wp_options');
+  await ctx.render('wp_options/wp_options');
 });
 
 module.exports = router;
